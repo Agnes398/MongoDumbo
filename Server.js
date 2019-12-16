daeb8ff91b01fe5588b3fe9c60c1e6f5dc68f1af
Neohack00 <neo@bylock.se>
2019-12-16 17:29:19 +0100

    GET data from db on /Notes

--- Server.js
+++ Server.js
@@ -9,13 +9,19 @@ var app = express();
 var port = process.env.PORT || 1337;
 var client = new MongoClient(URI, {useNewUrlParser:true , useUnifiedTopology:true});
 
+app.get('/notes' , function(req, res){
+    collection.find({}).toArray((err, result) => {
+        res.send(result);
+    })
+})
+
 app.get('/', function(req , res){
     res.send('I am alive');
 })
 app.listen(port , function(){
     console.log('Server is running at port: ' + port )
     client.connect(err => {
-        collection = client.db("MongoDumboDB").collection("DumbeCollection");
+        collection = client.db("MongoDumboDB").collection("DumboCollection");
         console.log("DB has been connected")
     });     
 });
