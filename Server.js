092edb9dcb8150201e8a6b5cf4d41a5d4320d1a6
Neohack00 <neo@bylock.se>
2019-12-17 12:37:54 +0100

    created html file and insert function

--- Server.js
+++ Server.js
@@ -3,25 +3,22 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const URI = "mongodb+srv://User01:User01@codecenter01010-zzf41.mongodb.net/test";
-
-
-
+var app = express();
 
 var collection;
-var app = express();
+
 var port = process.env.PORT || 1337;
 var client = new MongoClient(URI, {useNewUrlParser:true , useUnifiedTopology:true});
 var testdataOne ;
 var testdataTwo;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
 app.get('/notes' , function(req, res){
     collection.find({}).toArray((err, result) => {
         res.send(result);
     });
 });
-
 app.get('/notes/:id', function(req,res){
     collection.findOne({"_id": new ObjectId(req.params.id)} , (err, result) => {
         res.send(result);
@@ -29,7 +26,7 @@ app.get('/notes/:id', function(req,res){
 });
 
 app.get('/', function(req , res){
-    res.send('I am alive');
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.post('/add', function(req, res){
     var item = {
@@ -40,6 +37,8 @@ app.post('/add', function(req, res){
         res.send(item);
         console.log("Added new items to DB");
     })
+    res.redirect('/public/showdata.html');
+    
    
       
 })
