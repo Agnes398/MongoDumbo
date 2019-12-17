47ce732eb54ff86ddb12e108c8cebb82b2d038b9
Neohack00 <neo@bylock.se>
2019-12-17 14:26:14 +0100

    Redirect fixed

--- Server.js
+++ Server.js
@@ -9,16 +9,16 @@ var collection;
 
 var port = process.env.PORT || 1337;
 var client = new MongoClient(URI, {useNewUrlParser:true , useUnifiedTopology:true});
-var testdataOne ;
-var testdataTwo;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.get('/notes' , function(req, res){
     collection.find({}).toArray((err, result) => {
         res.send(result);
     });
 });
+
 app.get('/notes/:id', function(req,res){
     collection.findOne({"_id": new ObjectId(req.params.id)} , (err, result) => {
         res.send(result);
@@ -28,24 +28,22 @@ app.get('/notes/:id', function(req,res){
 app.get('/', function(req , res){
     res.sendFile(__dirname + '/public/index.html');
 });
+
 app.post('/add', function(req, res){
+    res.redirect('/')
     var item = {
         headline: req.body.headline,
         content: req.body.content
     };   
     collection.insertOne(item, function(err, result){
-        res.send(item);
         console.log("Added new items to DB");
-    });
-
-   
-      
-})
+    });     
+});
 
 app.listen(port , function(){
-    console.log('Server is running at port: ' + port )
+    console.log('Server is running at port: ' + port );
     client.connect(err => {
         collection = client.db("MongoDumboDB").collection("DumboCollection");
-        console.log("DB has been connected")
+        console.log("DB has been connected");
     });     
 });
